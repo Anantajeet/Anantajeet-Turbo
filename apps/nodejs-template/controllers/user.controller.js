f8bdef8ce83bcf7275d58df0228c2fc105e8fcff
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-07-10 12:30:02 +0530

    node bug fixes in controller

--- apps/nodejs-template/controllers/user.controller.js
+++ apps/nodejs-template/controllers/user.controller.js
@@ -1,8 +1,5 @@
 import { User } from "../models/user.model.js"
-import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
 
-import consola from "consola";
 import bcrypt from "bcrypt";
 
 export const signupUser = async (req, res) => {
@@ -14,21 +11,25 @@ export const signupUser = async (req, res) => {
         const existedUser = await User.findOne({ email })
 
         if (existedUser) {
-            res.status(400).json({ status: false, message: "User already existed" })
+            return res.status(400).json({ status: false, message: "User already existed" })
         }
 
         const hashpassword = await bcrypt.hash(password, 10)
 
-        const newuser = await User.create({
+        const newUser = await User.create({
             username,
             password: hashpassword,
             email
         })
 
-        return res.status(200).json(
-            res.status(200).json({ status: true, newuser, message: "User already existed" })
+        await newUser.save()
 
-        )
+        return res.status(200).json({
+            status: true,
+            newUser,
+            message: "User already existed"
+        })
+
     } catch (e) {
         res.status(500).json({ status: false, e, message: "Internal sever Errors" })
     }
